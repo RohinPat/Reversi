ba190d28cba55384bc29514a8ff5dee393c8172d
Rohin Patel <Rohinpatel1124@gmail.com>
2023-10-27 00:30:14 -0400

    Hopefully most of this works!!!!

--- src/Model/Board.java
+++ src/Model/Board.java
@@ -24,22 +24,17 @@ public class Board {
     }
   }
 
-  
-
-
   public void placeDisc(int row, int col, Disc disc) {
-    Cell c = grid.get(new Coordinate(row, col));
-    
+    grid.get(new Coordinate(row, col)).setContent(disc);
   }
 
-  
-  public Disc getDiscAt(int q, int r) {
-    
+  public Disc getDiscAt(int row, int col) {
+    return grid.get(new Coordinate(row, col)).getContent();
   }
 
-  
-  public boolean isCellEmpty(int q, int r) {
-    return grid[q][r].getDisc() == Disc.EMPTY;
+
+  public boolean isCellEmpty(int row, int col) {
+    return grid.get(new Coordinate(row, col)).getContent() == Disc.EMPTY;
   }
 
   
