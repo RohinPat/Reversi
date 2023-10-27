f8f870bad57ca688c28d302dfa9785a172543f14
Rohin Patel <Rohinpatel1124@gmail.com>
2023-10-27 16:27:34 -0400

    something

--- src/Model/Reversi.java
+++ src/Model/Reversi.java
@@ -2,6 +2,8 @@ package Model;
 
 public class Reversi {
   void startGame(int size) {
-    
+    // set up the starting peices on the board
+    // set up the players (Idk how to do this)
+    // 
   }
 }
