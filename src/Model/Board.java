31b619994f0afb55d55c2a7cf0b7f471c00d391f
akarr509 <anish.karre@gmail.com>
2023-10-28 15:38:33 -0400

    axial coordinate board setup
    implemented the construction of the intial baord with the right coordinate system

--- src/Model/Board.java
+++ src/Model/Board.java
@@ -9,16 +9,24 @@ public class Board {
 
   public Board(int size) {
     this.size = size;
-    int rowSize = (size * 2) - 1;
-    int colSize = (size * 2) - 1;
-    for (int row = 0; row < size; row++) {
-      for (int col = 0; col < size; col++) {
-        if (row > this.size) {
-          
+
+    // this first loop sets up the first half of rows not including the middle row
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = -i; j < size; j++) {
+            grid.put(new Coordinate(j, -(size - 1 - i), (-j + (size - 1 - i))), new Cell(Disc.EMPTY));
         }
-        grid.put(new Coordinate(row, col), new Cell(Disc.EMPTY));
+    }
+
+    // this second loop sets up the rest of the rows INCLUDING the middle row - intializes every cell to be empty at first
+    for (int i = 0; i < size; i ++){
+      for (int j = -(size - 1); j < size - 1; j++){
+        grid.put(new Coordinate(j, i, (-j-(i))), new Cell(Disc.EMPTY));
       }
     }
+
+    //i'm thinking of adding the starting pieces in the constructor as well but this should be easily moveable
+
+    
   }
 
   public void placeDisc(int row, int col, Disc disc) {
