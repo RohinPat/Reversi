38adfb78f633050153e24e16eacb8ac9de44f3c7
Rohin Patel <Rohinpatel1124@gmail.com>
2023-10-29 16:54:47 -0400

    Update BoardRenderer.java

--- src/View/BoardRenderer.java
+++ src/View/BoardRenderer.java
@@ -38,8 +38,18 @@ public class BoardRenderer {
           for (int j = -(boardSize - 1); j < boardSize - 1; j++) {
               Coordinate coord = new Coordinate(j, i);
               Cell cell = model.getGridCell(coord);  
-              String cellSymbol = cell.getDisc().toString();  
-              ap.append(" ").append(cellSymbol);
+              Disc cellSymbol = cell.getContent();  
+              String content = "";
+              if (cellSymbol == Disc.BLACK) {
+                content = "X";
+              }
+              else if (cellSymbol == Disc.BLACK) {
+                content = "O";
+              }
+              else {
+                content = "_";
+              }
+              ap.append(" ").append(content);
           }
   
           ap.append("\n");
@@ -47,17 +57,4 @@ public class BoardRenderer {
   
       return ap.toString();
   }
-  
 }
-for (int i = 0; i < board.size - 1; i++) {
-        for (int j = -i; j < board.size; j++) {
-            grid.put(new Coordinate(j, -(board.size - 1 - i)), new Cell(Disc.EMPTY));
-        }
-    }
-
-    // this second loop sets up the rest of the rows INCLUDING the middle row - intializes every cell to be empty at first
-    for (int i = 0; i < board.size; i ++){
-      for (int j = -(board.size - 1); j < board.size - 1; j++){
-        grid.put(new Coordinate(j, i), new Cell(Disc.EMPTY));
-      }
-    }
