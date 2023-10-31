0cee29b4dffbd3ddfa70a7ad97cc88268d8a2059
Rohin Patel <Rohinpatel1124@gmail.com>
2023-10-31 02:34:06 -0400

    Update TextualViewTests.java

--- test/TextualViewTests.java
+++ test/TextualViewTests.java
@@ -1,15 +1,15 @@
 
-import org.junit.Assert;
-import org.junit.Before;
+//import org.junit.Assert;
+//import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
+//import static org.junit.Assert.assertNotEquals;
+//import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
@@ -19,6 +19,10 @@ import model.Coordinate;
 import model.Disc;
 import view.BoardRenderer;
 
+/**
+ * Contains unit tests for validating the functionality of the game's textual 
+ * view and related operations.
+ */
 public class TextualViewTests {
   @Test
   public void testBoardIsCreatedCorrectly() {
@@ -174,6 +178,52 @@ public class TextualViewTests {
     assertEquals(c.getContent(), Disc.EMPTY);
   }
   
-  @Test
-  
+  /*
+   * Board Initialization:
+
+Test creating a board with negative or zero size. It should probably throw an exception.
+Test the state of the board right after initialization without calling playGame.
+Move Validations:
+
+Test making a move to an already occupied cell.
+Test making a move that doesn't result in capturing any opponent discs.
+Test making moves on the boundaries of the board.
+Test making a move outside the boundaries of the board.
+Game Progression:
+
+Simulate a complete game with alternating valid moves between players and verify the final state.
+Test the scenario where a player has no valid moves and is forced to pass.
+Renderer:
+
+Test BoardRenderer with a null board or appendable. It should handle or throw appropriate exceptions.
+If you plan on adding more rendering features (e.g., highlighting possible moves, showing scores), ensure you have tests for those.
+Coordinate:
+
+Test the equals method with non-Coordinate objects.
+Test the hashCode method's consistency. Given the same coordinate values, the hash code should be the same.
+Cell:
+
+Test changing the content of a cell multiple times.
+Test getting the content of a cell after various changes.
+Consecutive Passes:
+
+Test scenarios where players pass turns consecutively, but not enough to end the game.
+Capture Mechanics:
+
+Test the capturing mechanics in all possible directions (not just a straight line).
+Edge Cases:
+
+For methods that accept arguments, consider edge cases (e.g., null values, extreme values).
+Error Handling:
+
+Ensure that your methods throw the expected exceptions under erroneous conditions.
+Test how the classes handle unexpected states or inputs.
+Performance:
+
+While this isn't a unit test per se, consider performance tests especially if the board size can be large. This will help you ensure the game remains performable under larger scenarios.
+Integration Tests:
+
+While the provided tests are more unit-test in nature (testing individual components), consider writing tests that cover the interaction of multiple components. This can help catch issues that might not surface when components are tested in isolation.
+   */
+
 }
